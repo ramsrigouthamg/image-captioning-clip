c3d3e3c43a9bf6b3152602e5ff1c5c706a887318
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2022-09-08 00:00:25 +0530

    Update download.py

--- src/download.py
+++ src/download.py
@@ -3,11 +3,18 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import T5ForConditionalGeneration
+# from transformers import T5ForConditionalGeneration
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from huggingface_hub import hf_hub_download
+import clip
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    model = T5ForConditionalGeneration.from_pretrained('t5-small')
+    # model = T5ForConditionalGeneration.from_pretrained('t5-small')
+    model1 = GPT2LMHeadModel.from_pretrained("gpt2")
+    tokenizer1 = GPT2Tokenizer.from_pretrained("gpt2")
+    conceptual_weight = hf_hub_download(repo_id="akhaliq/CLIP-prefix-captioning-conceptual-weights", filename="conceptual_weights.pt")
+    clip_model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
 
 if __name__ == "__main__":
     download_model()
